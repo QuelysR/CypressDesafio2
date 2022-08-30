4d343d1a2b19dcd3e3a71927c19434069b2e1987
Quelys Rodriguez <quelysrodriguez@MacBook-Air-de-Quelys.local>
2022-08-29 21:42:55 -0300

    arreglos de test

--- cypress/e2e/desafio2.cy.js
+++ cypress/e2e/desafio2.cy.js
@@ -6,31 +6,19 @@ describe('Hooks', () => {
         cy.fixture("loginData").then(datos => {
             datosLogin = datos;
         })
-        cy.fixture("registrodata").then(datos => {
-            datosRegistro = datos;
-        })
-        numeroRandom = Math.floor(Math.random() * 1000)
+
     })
     beforeEach("Before each", () => {
         cy.visit("/")
-    })
-
-
-    it("Registro de Usuario", () =>{
-        cy.get("#user").type(datosRegistro.PrimerUsuario.usuario + numeroRandom);
-        cy.get("#pass").type(datosRegistro.PrimerUsuario.contraseña);
-        cy.get(`[value='${datosRegistro.PrimerUsuario.genero}']`).check({force:true})
-        cy.xpath("//button[contains(text(), 'Register')]").click();
-        cy.get(`[id*='user_${datosRegistro.PrimerUsuario.usuario + numeroRandom}_']`).should("be.visible")
-    })
-
-    it('Ingresar al sistema', () => {
         cy.get("#registertoggle").dblclick();
-        cy.get("#user").type(datosLogin.usuario + numeroRandom);
+        cy.get("#user").type(datosLogin.usuario);
         cy.get("#pass").type(datosLogin.contraseña);
         cy.xpath("//button[contains(text(), 'Log in')]").click();
-        cy.get(`[id*='user_${datosLogin.usuario + numeroRandom}_']`).should("be.visible")
         cy.get('#todolistlink').click()
+    })
+
+
+    it('Crear Tareas', () => {
         let tarea1 = 'tarea1'
         let tarea2 = 'tarea2'
         let tarea3 = 'tarea3'
@@ -41,15 +29,31 @@ describe('Hooks', () => {
         cy.get('#task').type(`${tarea3}{enter}`)
         cy.get('#task').type(`${tarea4}{enter}`)
         cy.get('#task').type(`${tarea5}{enter}`)
+    });
+    it('Validar si existen botones', () => {
         cy.get('#all').should('exist');
         cy.get('#completed').should('exist');
         cy.get('#active').should('exist');
         cy.get('#removeAll').should('exist');
+    });
+
+    it('Crear tarea y Completarla', () => {
+        let tarea1 = 'tarea1'
+        cy.get('#task').type(`${tarea1}{enter}`)
         cy.get('p').contains(`${tarea1}`).click()
         cy.xpath("//p[contains(text(), 'tarea1')]//following-sibling::button").click()
-        cy.xpath("//p[contains(text(), 'tarea2')]//following-sibling::button").click()       
     });
+
+      
+    it('Crear tarea y eliminarla', () => {
+        let tarea1 = 'tarea1'
+        cy.get('#task').type(`${tarea1}{enter}`)
+        cy.xpath("//p[contains(text(), 'tarea1')]//following-sibling::button").click()
+    });
+
     after("After", () => {
         cy.log("After")
     })
+
+    //button[contains(text(), 'continue')]//child::div")
 })
